f5a7e6e397b95bb44d83ce6c05e188378dec7b6d
Kunal Srivastav <kunalsrivastavgs2001@gmail.com>
2021-11-19 15:43:58 +0530

    All Bugs related to SignIn by OTP & SignIn by Google Fixed.

--- app/src/main/java/com/gla/medresto/HomeActivity.java
+++ app/src/main/java/com/gla/medresto/HomeActivity.java
@@ -21,12 +21,9 @@ import com.google.firebase.auth.FirebaseUser;
 import org.jetbrains.annotations.NotNull;
 
 public class HomeActivity extends AppCompatActivity {
-    Button b1;
+    Button logoutBtn;
     FirebaseAuth firebaseAuth;
     SharedPreferences sp;
-
-
-  Button b2;
 
     GoogleSignInClient googleSignInClient;
 
@@ -34,45 +31,46 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        b1=(Button) findViewById(R.id.button);
-        sp = getSharedPreferences("login",MODE_PRIVATE);
-        b2 = (Button)findViewById(R.id.button2);
+        logoutBtn = (Button) findViewById(R.id.logoutBtn);
+        sp = getSharedPreferences("login", MODE_PRIVATE);
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
         googleSignInClient = GoogleSignIn.getClient(HomeActivity.this, GoogleSignInOptions.DEFAULT_SIGN_IN);
 
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull @NotNull Task<Void> task) {
-                        if(task.isSuccessful())
-                        {
-                            Toast.makeText(HomeActivity.this, "logout", Toast.LENGTH_SHORT).show();
-                            firebaseAuth.signOut();
-                            finish();
-                        }
-                    }
-                });
-            }
-        });
-
-
-
-
-        b1.setOnClickListener(new View.OnClickListener() {
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                firebaseAuth.signOut();
-                Intent  i =new Intent(HomeActivity.this,RegisterActivity.class);
-//                firebaseAuth.signOut();
-                sp.edit().putBoolean("loggedIn",false).apply();
-                sp.edit().putString("type", null).apply();
-                sp.edit().putString("number",null).apply();
-                startActivity(i);
-                finish();
+                if (sp.getString("type", "").equals("googleLogin")) {
+                    googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull @NotNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                sp.edit().putBoolean("loggedIn", false).apply();
+                                sp.edit().putString("type", null).apply();
+                                sp.edit().putString("gmail", null).apply();
+                                firebaseAuth.signOut();
+                                Toast.makeText(HomeActivity.this, "Logout Successful", Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(HomeActivity.this, RegisterActivity.class);
+                                startActivity(i);
+                                finish();
+                            }
+                        }
+                    });
+                } else if (sp.getString("type", "").equals("phoneLogin")) {
+                    sp.edit().putBoolean("loggedIn", false).apply();
+                    sp.edit().putString("type", null).apply();
+                    sp.edit().putString("number", null).apply();
+                    firebaseAuth.signOut();
+                    Toast.makeText(HomeActivity.this, "Logout Successful", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(HomeActivity.this, RegisterActivity.class);
+                    startActivity(i);
+                    finish();
+                } else {
+                    Toast.makeText(HomeActivity.this, "Something went wrong, please try again later.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+
     }
 }
--- app/src/main/java/com/gla/medresto/RegisterActivity.java
+++ app/src/main/java/com/gla/medresto/RegisterActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -18,7 +17,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -35,172 +33,16 @@ import com.google.firebase.auth.PhoneAuthProvider;
 import java.util.concurrent.TimeUnit;
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText phoneNumber;
-    private EditText otp;
+    private static boolean otpRequested = false;
     Button getOtp;
     Button register;
-//    ImageView googleSignIn;
     SharedPreferences sp;
     String verificationCodeBySystem;
-    private static boolean otpRequested = false;
-
-
-
     ImageView signInButton;
     GoogleSignInClient googleSignInClient;
     FirebaseAuth firebaseAuth;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-
-        sp = getSharedPreferences("login", MODE_PRIVATE);
-
-        phoneNumber = findViewById(R.id.inputNumber);
-        otp = findViewById(R.id.inputOTP);
-        getOtp = findViewById(R.id.getOTPButton);
-        register = findViewById(R.id.registerButton);
-
-
-
-
-        signInButton = (ImageView) findViewById(R.id.googleRegister);
-        GoogleSignInOptions googleSignInOptions=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken("413645942786-b8caijhi98b0nu2vubbvgcvfru7b2fl0.apps.googleusercontent.com").requestEmail().build();
-        googleSignInClient= GoogleSignIn.getClient(RegisterActivity.this,googleSignInOptions);
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=googleSignInClient.getSignInIntent();
-                startActivityForResult(i,100);
-            }
-        });
-        firebaseAuth=FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
-        if(firebaseUser!=null)
-        {
-            Intent i=new Intent(RegisterActivity.this,HomeActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(i);
-            finish();
-        }
-
-
-
-
-
-
-
-
-
-
-        getOtp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (phoneNumber.getText().toString().length() != 10) {
-                    Toast.makeText(RegisterActivity.this, "Invalid Phone Number\nPlease Check your Phone Number", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                otpRequested = true;
-                sendOtpToUser(phoneNumber.getText().toString());
-            }
-        });
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!otpRequested) {
-                    Toast.makeText(RegisterActivity.this, "Please request for OTP first.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                String code = otp.getText().toString();
-
-                if (code.isEmpty()) {
-                    Toast.makeText(RegisterActivity.this, "Please Enter Otp", Toast.LENGTH_SHORT).show();
-                    return;
-                } else if (code.length() < 6) {
-                    Toast.makeText(RegisterActivity.this, "Wrong OTP...", Toast.LENGTH_SHORT).show();
-                    otp.requestFocus();
-                    return;
-                }
-                verifyCode(code);
-            }
-        });
-
-//        googleSignIn = (ImageView) findViewById(R.id.googleRegister);
-//        googleSignIn.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                // Method to login by Google.
-//                Toast.makeText(RegisterActivity.this, "Clicked...", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-    }
-
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==100)
-        {
-            Task<GoogleSignInAccount> signInAccountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
-            if(signInAccountTask.isSuccessful())
-            {
-                Toast.makeText(this, "sign in successful", Toast.LENGTH_SHORT).show();
-                try{
-                    GoogleSignInAccount googleSignInAccount=signInAccountTask.getResult(ApiException.class);
-                    if(googleSignInAccount!=null)
-                    {
-                        AuthCredential authCredential= GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(),null);
-                        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<AuthResult> task) {
-                                if(task.isSuccessful())
-                                {
-                                    Toast.makeText(RegisterActivity.this, "firebase login successful", Toast.LENGTH_SHORT).show();
-                                    Intent j=new Intent(RegisterActivity.this,HomeActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                    startActivity(j);
-                                }
-                                else{
-                                    Toast.makeText(RegisterActivity.this, "database  updated", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-                    }
-                }
-                catch (ApiException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private void sendOtpToUser(String phoneNumber) {
-        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(FirebaseAuth.getInstance())
-                        .setPhoneNumber("+91" + phoneNumber)       // Phone number to verify
-                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
-                        .setActivity(this)                 // Activity (for callback binding)
-                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
-                        .build();
-        PhoneAuthProvider.verifyPhoneNumber(options);
-    }
-
+    private EditText phoneNumber;
+    private EditText otp;
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
         @Override
@@ -224,6 +66,115 @@ public class RegisterActivity extends AppCompatActivity {
         }
     };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        sp = getSharedPreferences("login", MODE_PRIVATE);
+
+        phoneNumber = findViewById(R.id.inputNumber);
+        otp = findViewById(R.id.inputOTP);
+        getOtp = findViewById(R.id.getOTPButton);
+        register = findViewById(R.id.registerButton);
+
+
+        signInButton = (ImageView) findViewById(R.id.googleRegister);
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken("413645942786-b8caijhi98b0nu2vubbvgcvfru7b2fl0.apps.googleusercontent.com").requestEmail().build();
+        googleSignInClient = GoogleSignIn.getClient(RegisterActivity.this, googleSignInOptions);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = googleSignInClient.getSignInIntent();
+                startActivityForResult(i, 100);
+            }
+        });
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser != null) {
+            Intent i = new Intent(RegisterActivity.this, HomeActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
+            finish();
+        }
+
+
+        getOtp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (phoneNumber.getText().toString().length() != 10) {
+                    Toast.makeText(RegisterActivity.this, "Invalid Phone Number, Please Check your Phone Number.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                otpRequested = true;
+                sendOtpToUser(phoneNumber.getText().toString());
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!otpRequested) {
+                    Toast.makeText(RegisterActivity.this, "Please request for OTP first.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String code = otp.getText().toString();
+
+                if (code.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Please Enter OTP", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (code.length() < 6) {
+                    Toast.makeText(RegisterActivity.this, "Wrong OTP...", Toast.LENGTH_SHORT).show();
+                    otp.requestFocus();
+                    return;
+                }
+                verifyCode(code);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Task<GoogleSignInAccount> signInAccountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+            if (signInAccountTask.isSuccessful()) {
+                try {
+                    GoogleSignInAccount googleSignInAccount = signInAccountTask.getResult(ApiException.class);
+                    if (googleSignInAccount != null) {
+                        AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
+                        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    sp.edit().putBoolean("loggedIn", true).apply();
+                                    sp.edit().putString("type", "googleLogin").apply();
+                                    sp.edit().putString("gmail", googleSignInAccount.getEmail()).apply();
+                                    Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                    Intent j = new Intent(RegisterActivity.this, HomeActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(j);
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "Database Updated", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                } catch (ApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void sendOtpToUser(String phoneNumber) {
+        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(FirebaseAuth.getInstance())
+                .setPhoneNumber("+91" + phoneNumber)       // Phone number to verify
+                .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                .setActivity(this)                 // Activity (for callback binding)
+                .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
 
     private void verifyCode(String codeByUser) {
 
@@ -242,9 +193,8 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if (task.isSuccessful()) {
-
-                            Toast.makeText(RegisterActivity.this, "Your Account has been created successfully!", Toast.LENGTH_SHORT).show();
-                            sp.edit().putBoolean("loggedIn",true).apply();
+                            Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            sp.edit().putBoolean("loggedIn", true).apply();
                             sp.edit().putString("type", "phoneLogin").apply();
                             sp.edit().putString("number", phoneNumber.getText().toString()).apply();
                             //Perform Your required action here to either let the user sign In or do something required
